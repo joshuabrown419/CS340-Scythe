03dd784963dd4ff1bbd9e5655b97ec2e251bde1a
noahdude1203 <gissn@oregonstate.edu>
2023-05-10 11:51:05 -0700

    Web Page Outline

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var path = require('path');
 var express = require('express');
 var expresshbs = require('express-handlebars')
 const { nextTick } = require('process');
-console.log(postData)
 
 var app = express();
 var port = process.env.PORT || 3988;
@@ -32,4 +31,21 @@ app.get('/factions', function (req, res) {
     res.status(200).render('factions', {
       pageTitle: "Scythe Faction Database"
     })
+});
+app.get('/insert', function (req, res) {
+  res.status(200).render('insert', {
+    pageTitle: "Insert Scythe Game:"
+  })
+});
+
+app.use(express.static('public'));
+
+app.get('*', function (req, res) {
+  res.status(404).render('404',{
+    pageTitle: "Scythe Request Error"
+  })
+});
+
+app.listen(port, function () {
+  console.log("== Server is listening on port", port);
 });
