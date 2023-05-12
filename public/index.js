c8428ba89af851cffbb41e7ee317b404f33594a5
noahdude1203 <114532160+noahdude1203@users.noreply.github.com>
2023-05-11 20:15:46 -0700

    Noahs branch (#4)
    * webpage start
    
    * Web Page Outline
    
    * Adding tables
    
    * Completed Step 3 Handlebars
    
    * Update main.handlebars
    
    * Create .gitignore
    
    * Update .gitignore
    
    * Update .gitignore
    
    * Remove Node_Modules
    
    * Update main.handlebars
    
    * Fixed links linking to localhost
    
    ---------
    
    Co-authored-by: Joshua Brown <brownjo8@oregonstate.edu>

---
+++ public/index.js
@@ -0,0 +1,28 @@
+//click links to take you from page to page
+
+var pageURL = "/"
+
+var homePage = document.getElementById("home-link") 
+
+var gamePage = document.getElementById("game-link") 
+var factionsPage = document.getElementById("factions-link") 
+var playerPage = document.getElementById("player-link") 
+var insertPage = document.getElementById("insert-link") 
+
+
+homePage.addEventListener('click', function(){
+    window.location.href = pageURL
+})
+
+gamePage.addEventListener('click', function(){
+    window.location.href = pageURL+"game"
+})
+factionsPage.addEventListener('click', function(){
+    window.location.href = pageURL+"factions"
+})
+playerPage.addEventListener('click', function(){
+    window.location.href = pageURL+"player"
+})
+insertPage.addEventListener('click', function(){
+    window.location.href = pageURL+"insert"
+})
---
+++ server.js
@@ -0,0 +1,53 @@
+var path = require('path');
+var express = require('express');
+var expresshbs = require('express-handlebars')
+const { nextTick } = require('process');
+var playerData = require('./playerData.json');
+
+var app = express();
+var port = process.env.PORT || 3988;
+
+app.engine('handlebars', expresshbs.engine({
+    defaultLayout: "main"
+  }))
+
+app.set('view engine', 'handlebars')
+
+app.get('/', function (req, res) {
+    res.status(200).render('index', {
+      pageTitle: "Scythe Database"
+    })
+});
+app.get('/game', function (req, res) {
+    res.status(200).render('game', {
+      pageTitle: "Scythe Game Overview"
+    })
+});
+app.get('/player', function (req, res) {
+    res.status(200).render('player', {
+      pageTitle: "Scythe Player Database"
+    })
+});
+app.get('/factions', function (req, res) {
+    res.status(200).render('factions', {
+      pageTitle: "Scythe Faction Database"
+    })
+});
+app.get('/insert', function (req, res) {
+  res.status(200).render('insert', {
+    pageTitle: "Insert Scythe Game:",
+    playerCount: playerData
+  })
+});
+
+app.use(express.static('public'));
+
+app.get('*', function (req, res) {
+  res.status(404).render('404',{
+    pageTitle: "Scythe Request Error"
+  })
+});
+
+app.listen(port, function () {
+  console.log("== Server is listening on port", port);
+});
