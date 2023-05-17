bb0ec0c8924dfa8e05a7d2546078680e1949f0d7
noahdude1203 <gissn@oregonstate.edu>
2023-05-17 11:51:46 -0700

    NAMECHANGE

--- public/index.js
+++ public/index.js
@@ -33,19 +33,21 @@ insertPage.addEventListener('click', function(){
 //buttons
 
 for (var i = 1; i <= 7; i++){
-    var classButton = document.getElementById("toggle-player-"+i)
+    let classButton = document.getElementById("toggle-player-"+i)
     console.log("classButton: "+ classButton)
     let hidden = false
     const textBox = document.getElementById("player-"+i+"-text")
     classButton.addEventListener('click', function(){
         console.log("hidden: "+ hidden)
-	console.log("textBox: " + textBox)
+	    console.log("textBox: " + textBox)
         if (hidden){
             textBox.removeAttribute("hidden")
             hidden = false
+            classButton.textContent= "NONE"
         }else{
             textBox.setAttribute("hidden", "true")
             hidden = true
+            classButton.textContent= "ADD"
         }
     })
 }
