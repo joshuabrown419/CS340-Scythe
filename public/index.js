85e4abcd67358030ca3d1117c898877c85056dde
noahdude1203 <gissn@oregonstate.edu>
2023-05-24 10:18:20 -0700

    Db-connectors

--- public/index.js
+++ public/index.js
@@ -12,18 +12,23 @@ var insertPage = document.getElementById("insert-link")
 
 
 homePage.addEventListener('click', function(){
+    // removeToggleButtons()
     window.location.href = pageURL
 })
 gamePage.addEventListener('click', function(){
+    // removeToggleButtons()
     window.location.href = pageURL+"game"
 })
 factionsPage.addEventListener('click', function(){
+    // removeToggleButtons()
     window.location.href = pageURL+"factions"
 })
 playerPage.addEventListener('click', function(){
+    // removeToggleButtons()
     window.location.href = pageURL+"player"
 })
 boardPage.addEventListener('click', function(){
+    // removeToggleButtons()
     window.location.href = pageURL+"board"
 })
 insertPage.addEventListener('click', function(){
@@ -37,17 +42,28 @@ for (var i = 1; i <= 7; i++){
     console.log("classButton: "+ classButton)
     let hidden = false
     const textBox = document.getElementById("player-"+i+"-text")
-    classButton.addEventListener('click', function(){
-        console.log("hidden: "+ hidden)
-	    console.log("textBox: " + textBox)
-        if (hidden){
-            textBox.removeAttribute("hidden")
-            hidden = false
-            classButton.textContent= "NONE"
-        }else{
-            textBox.setAttribute("hidden", "true")
-            hidden = true
-            classButton.textContent= "ADD"
-        }
-    })
+    if (classButton != null){
+        classButton.addEventListener('click', function(){
+            console.log("hidden: "+ hidden)
+            console.log("textBox: " + textBox)
+            if (hidden){
+                textBox.removeAttribute("hidden")
+                hidden = false
+                classButton.textContent= "NONE"
+            }else{
+                textBox.setAttribute("hidden", "true")
+                hidden = true
+                classButton.textContent= "ADD"
+            }
+        })
+    }
 }
+
+// function removeToggleButtons(){
+//     for (var i = 1; i <= 7; i++){
+//         let classButton = document.getElementById("toggle-player-"+i)
+//         classButton.removeEventListener("click", function(){
+//             console.log("removed listener "+i)
+//         }) 
+//     }
+// }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var expresshbs = require('express-handlebars')
 const { nextTick } = require('process');
 var playerData = require('./playerData.json');
+var db = require('./database-connector')
 
 var app = express();
 var port = process.env.PORT || 3988;
@@ -17,32 +18,76 @@ app.get('/', function (req, res) {
     res.status(200).render('index', {
       pageTitle: "Scythe Database"
     })
+
+
 });
 app.get('/game', function (req, res) {
     res.status(200).render('game', {
       pageTitle: "Scythe Game Overview"
     })
+    //insert db statements for individual game statistics
+    // Define our queries
+    query1 = 'DROP TABLE IF EXISTS diagnostic;';
+    query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
+    query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working!")';
+    query4 = 'SELECT * FROM diagnostic;';
+
+    // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+
+    // DROP TABLE...
+    db.pool.query(query1, function (err, results, fields){
+
+        // CREATE TABLE...
+        db.pool.query(query2, function(err, results, fields){
+
+            // INSERT INTO...
+            db.pool.query(query3, function(err, results, fields){
+
+                // SELECT *...
+                db.pool.query(query4, function(err, results, fields){
+
+                    // Send the results to the browser
+                    let base = "<h1>MySQL Results:</h1>"
+                    res.send(base + JSON.stringify(results));
+                });
+            });
+        });
+    });
+    
+
 });
 app.get('/player', function (req, res) {
     res.status(200).render('player', {
       pageTitle: "Scythe Player Database"
     })
+    //insert db statements for player statistics
+
+
 });
 app.get('/factions', function (req, res) {
     res.status(200).render('factions', {
       pageTitle: "Scythe Faction Database"
     })
+    //insert db statements for each factions statistics
+
+
 });
 app.get('/board', function (req, res) {
   res.status(200).render('board', {
     pageTitle: "Scythe Board Setup"
   })
+  //insert db statements for each board setup and id
+
+
 });
 app.get('/insert', function (req, res) {
   res.status(200).render('insert', {
     pageTitle: "Insert Scythe Game:",
     playerCount: playerData
   })
+  //db insert statements to add a game to the db
+
+
 });
 
 app.use(express.static('public'));
